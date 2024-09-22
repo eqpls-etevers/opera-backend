1b31d2e82af1e69a30ef515ef7fbb4895434b711
jzide <jzide@JzWork>
2024-09-22 19:38:09 +0900

    update

--- aria/service/controls.py
+++ aria/service/controls.py
@@ -11,8 +11,10 @@ import re
 import uuid
 import base64
 
-from common import getRandomString, MeshControl, AsyncRest
+from common import getRandomString, MeshControl, AsyncRest, EpException
 from schema.aria import Endpoint
+
+from driver.keyclock import KeyCloak
 
 
 #===============================================================================
@@ -23,13 +25,16 @@ class Control(MeshControl):
     def __init__(self, path):
         MeshControl.__init__(self, path, sessionChecker='uerp')
 
+        self.keycloak = KeyCloak(self.config)
+
+        self.tenant = self.config['default']['tenant']
         self.domain = self.config['default']['domain']
         self.endpoint = self.config['default']['endpoint']
-        
+
         uerpHostname = self.config['uerp']['hostname']
         uerpHostport = self.config['uerp']['hostport']
         self.uerpEndpoint = f'http://{uerpHostname}:{uerpHostport}'
-        
+
         self.operaClientId = self.endpoint.replace('.', '-')
         self.operaHomeUrl = f'https://{self.endpoint}'
         self.operaLoginUri = f'{self.uri}/auth/login'
@@ -43,34 +48,145 @@ class Control(MeshControl):
         self.vidmAdminHeaders = {
             'Authorization': 'Basic ' + base64.b64encode(f'{self.vidmClientId}:{self.vidmClientSecret}'.encode('ascii')).decode('ascii')
         }
-        
+
         self.aaClientPrefix = self.config['aria']['aa_client_prefix']
         self.aaMap = {}
-        
+
     async def startup(self):
         await self.registerModel(Endpoint, 'uerp')
+        await self.keycloak.connect()
         await self.initAriaBackends()
 
     async def shutdown(self): pass
 
     def generateUuid4(self): return str(uuid.uuid4())
-    
+
     async def initAriaBackends(self):
         async with AsyncRest(self.vidmBaseUrl) as req:
             vidmAccessToken = (await req.post('/SAAS/auth/oauthtoken?grant_type=client_credentials', headers=self.vidmAdminHeaders))['access_token']
             vidmBearerToken = f'Bearer {vidmAccessToken}'
-            headers = {
-                'Authorization': vidmBearerToken
-            }
-            
+            headers = {'Authorization': vidmBearerToken}
+
             vidmOperaClient = None
             vidmAaClientIds = []
             for client in (await req.get('/SAAS/jersey/manager/api/oauth2clients', headers=headers))['items']:
                 clientId = client['clientId']
                 if self.endpoint == clientId: vidmOperaClient = client
                 elif self.aaClientPrefix in clientId and client['scope'] == 'user openid email profile': vidmAaClientIds.append(client['clientId'])
-            
+
             if not vidmOperaClient:
+                realmId = self.tenant
+                kcIdpMetadata = await self.keycloak.get(f'/realms/{realmId}/protocol/saml/descriptor')
+                LOG.DEBUG(kcIdpMetadata)
+
+                await self.keycloak.post(f'/admin/realms/{realmId}/client-scopes', {
+                    'name': 'vidm-scope',
+                    'description': 'vidm-scope',
+                    'type': 'default',
+                    'protocol': 'saml',
+                    'attributes': {
+                        'consent.screen.text': '',
+                        'display.on.consent.screen': True,
+                        'include.in.token.scope': True,
+                        'gui.order': ''
+                    }
+                })
+                for scope in await self.keycloak.get(f'/admin/realms/{realmId}/client-scopes'):
+                    if scope['name'] == 'vidm-scope': scopeId = scope['id']; break
+                else: raise EpException(404, 'Could not find client scope')
+                await self.keycloak.post(f'/admin/realms/{realmId}/client-scopes/{scopeId}/protocol-mappers/models', {
+                    'name': 'userName',
+                    'protocol': 'saml',
+                    'protocolMapper': 'saml-user-property-mapper',
+                    'config': {
+                        'friendly.name': 'userName',
+                        'attribute.name': 'userName',
+                        'attribute.nameformat': 'Basic',
+                        'user.attribute': 'username'
+                    }
+                })
+                await self.keycloak.post(f'/admin/realms/{realmId}/client-scopes/{scopeId}/protocol-mappers/models', {
+                    'name': 'email',
+                    'protocol': 'saml',
+                    'protocolMapper': 'saml-user-property-mapper',
+                    'config': {
+                        'friendly.name': 'email',
+                        'attribute.name': 'email',
+                        'attribute.nameformat': 'Basic',
+                        'user.attribute': 'email'
+                    }
+                })
+                await self.keycloak.post(f'/admin/realms/{realmId}/client-scopes/{scopeId}/protocol-mappers/models', {
+                    'name': 'surname',
+                    'protocol': 'saml',
+                    'protocolMapper': 'saml-user-property-mapper',
+                    'config': {
+                        'friendly.name': 'surname',
+                        'attribute.name': 'lastName',
+                        'attribute.nameformat': 'Basic',
+                        'user.attribute': 'lastName'
+                    }
+                })
+                await self.keycloak.post(f'/admin/realms/{realmId}/client-scopes/{scopeId}/protocol-mappers/models', {
+                    'name': 'givenName',
+                    'protocol': 'saml',
+                    'protocolMapper': 'saml-user-property-mapper',
+                    'config': {
+                        'friendly.name': 'givenName',
+                        'attribute.name': 'firstName',
+                        'attribute.nameformat': 'Basic',
+                        'user.attribute': 'firstName'
+                    }
+                })
+
+                await self.keycloak.post(f'/admin/realms/{realmId}/clients', {
+                    'clientId': f'https://{self.vidm_hostname}/SAAS/API/1.0/GET/metadata/sp.xml',
+                    'name': 'vidm',
+                    'description': 'vidm',
+                    'protocol': 'saml',
+                    'publicClient': True,
+                    'rootUrl': f'https://{self.vidm_hostname}',
+                    'baseUrl': f'https://{self.vidm_hostname}',
+                    'adminUrl': f'https://{self.vidm_hostname}/SAAS/auth/saml/response',
+                    'redirectUris': ['*'],
+                    'authorizationServicesEnabled': False,
+                    'serviceAccountsEnabled': False,
+                    'implicitFlowEnabled': False,
+                    'directAccessGrantsEnabled': True,
+                    'standardFlowEnabled': True,
+                    'frontchannelLogout': True,
+                    'alwaysDisplayInConsole': True,
+                    'attributes': {
+                        'saml_idp_initiated_sso_url_name': '',
+                        'saml_idp_initiated_sso_relay_state': '',
+                        'post.logout.redirect.uris': '+'
+                    }
+                })
+                for client in await self.keycloak.get(f'/admin/realms/{realmId}/clients'):
+                    if client['name'] == 'vidm':
+                        clientId = client['id'];
+                        client['frontchannelLogout'] = False
+                        client['attributes']['saml.client.signature'] = 'false'
+                        client['attributes']['saml.server.signature.keyinfo.xmlSigKeyInfoKeyNameTransformer'] = 'KEY_ID'
+                        client['attributes']['saml.encrypt'] = 'false'
+                        client['attributes']['logoUri'] = ''
+                        client['attributes']['policyUri'] = ''
+                        client['attributes']['tosUri'] = ''
+                        client['attributes']['saml_assertion_consumer_url_post'] = f'https://{self.vidm_hostname}/SAAS/auth/saml/response'
+                        client['attributes']['saml_assertion_consumer_url_redirect'] = f'https://{self.vidm_hostname}/SAAS/auth/saml/response'
+                        client['attributes']['saml_single_logout_service_url_post'] = ''
+                        client['attributes']['saml_single_logout_service_url_redirect'] = ''
+                        client['attributes']['saml_single_logout_service_url_soap'] = ''
+                        client['attributes']['saml_single_logout_service_url_artifact'] = ''
+                        client['attributes']['saml_artifact_binding_url'] = ''
+                        client['attributes']['saml_artifact_resolution_service_url'] = ''
+                        client['attributes']['saml.assertion.lifespan'] = ''
+                        client['authenticationFlowBindingOverrides']['browser'] = ''
+                        await self.keycloak.put(f'/admin/realms/opera/clients/{clientId}', client)
+                        await self.keycloak.put(f'/admin/realms/opera/clients/{clientId}/default-client-scopes/{scopeId}')
+                        break
+                else: raise EpException(404, 'Could not find client')
+
                 secret = getRandomString(16)
                 async with AsyncRest(self.vidmBaseUrl) as req:
                     vidmOperaClient = await req.post('/SAAS/jersey/manager/api/oauth2clients', headers={
@@ -96,6 +212,68 @@ class Control(MeshControl):
                         'inheritanceAllowed': False,
                         'returnFailureResponse': False
                     })
+
+                    orgNetworks = await req.get('/SAAS/jersey/manager/api/orgnetworks', headers={
+                        'Authorization': vidmBearerToken,
+                        'Accept': 'application/vnd.vmware.horizon.manager.orgnetwork.list+json'
+                    })['items']
+                    for orgNetwork in orgNetworks:
+                        if orgNetwork['name'] == 'ALL RANGES':
+                            allRange = orgNetwork
+                            allRange.pop('_links')
+                            break
+                    else: raise Exception('could not find ALL RANGES org network in vidm')
+
+                    vidmJitDir = await req.post('/SAAS/jersey/manager/api/connectormanagement/directoryconfigs', headers={
+                        'Authorization': vidmBearerToken,
+                        'Content-Type': 'application/vnd.vmware.horizon.manager.connector.management.directory.jit+json',
+                        'Accept': 'application/vnd.vmware.horizon.manager.connector.management.directory.jit+json'
+                    }, json={
+                        'name': 'opera',
+                        'domains': ['portal.lab']
+                    })
+
+                    await req.post('/SAAS/jersey/manager/api/identityProviders', headers={
+                        'Authorization': vidmBearerToken,
+                        'Content-Type': 'application/vnd.vmware.horizon.manager.external.identityprovider+json',
+                        'Accept': 'application/vnd.vmware.horizon.manager.external.identityprovider+json'
+                    }, json={
+                        'authMethods': [{
+                            'authMethodId': 0,
+                            'authScore': 1,
+                            'defaultMethod': False,
+                            'authMethodOrder': 0,
+                            'authMethodName': 'opera',
+                            'samlAuthnContext': 'urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified'
+                        }],
+                        'identityProviderType': 'MANUAL',
+                        'nameIdFormatType': 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+                        'friendlyName': 'opera',
+                        'metaData': kcIdpMetadata,
+                        'preferredBinding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+                        'jitEnabled': True,
+                        'directoryConfigurations': [{
+                            'type': 'JIT_DIRECTORY',
+                            'name': 'opera',
+                            'directoryId': vidmJitDir['directoryConfigurationId'],
+                            'userstoreId': vidmJitDir['userStoreId'],
+                            'countDomains': 1,
+                            'deleteInProgress': False,
+                            'syncConfigurationEnabled': False
+                        }],
+                        'customAttributeMappings': None,
+                        'nameIdFormatAttributeMappings': {
+                            'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified': 'userName',
+                            'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress': 'emails',
+                            'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent': 'id',
+                            'urn:oasis:names:tc:SAML:2.0:nameid-format:transient': 'userName'
+                        },
+                        'orgNetworks': [allRange],
+                        'description': '',
+                        'nIDPStatus': 1,
+                        'idpURL': None,
+                        'name': 'opera'
+                    })
             else:
                 async with AsyncRest(self.vidmBaseUrl) as req:
                     vidmOperaClient = await req.get(f'/SAAS/jersey/manager/api/oauth2clients/{vidmOperaClient["clientId"]}', headers=headers)
@@ -103,7 +281,7 @@ class Control(MeshControl):
             self.vidmOperaHeaders = {
                 'Authorization': 'Basic ' + base64.b64encode(f'{self.endpoint}:{self.vidmOperaSecret}'.encode('ascii')).decode('ascii')
             }
-            
+
             async with AsyncRest(self.vidmBaseUrl) as req:
                 for clientId in vidmAaClientIds:
                     client = await req.get(f'/SAAS/jersey/manager/api/oauth2clients/{clientId}', headers=headers)
@@ -113,7 +291,7 @@ class Control(MeshControl):
                         'clientId': clientId,
                         'redirectUri': redirectUri
                     }
-    
+
     def login(self):
         return f'https://{self.vidmHostname}/SAAS/auth/oauth2/authorize?domain={self.domain}&response_type=code&state={self.generateUuid4()}&client_id={self.endpoint}&redirect_uri={self.operaRedirectUrl}'
 
@@ -155,4 +333,4 @@ class Control(MeshControl):
                 'regions': regions
             })
         return endpoint['id']
-    
+
