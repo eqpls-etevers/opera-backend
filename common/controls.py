05db8b3744a55e1853a493473cf089a03ffe2615
jzide <jzide@JzBook>
2024-09-24 11:13:18 +0900

    update

--- common/controls.py
+++ common/controls.py
@@ -485,6 +485,12 @@ class UerpControl(BaseControl):
         qFilter = ' AND '.join(qFilter)
         if filter: filter = f'{qFilter} AND ({filter})'
         else: filter = qFilter
+        
+        LOG.DEBUG(authInfo.checkAdmin())
+        LOG.DEBUG(schemaInfo.aaa)
+        LOG.DEBUG(schemaInfo.sref)
+        LOG.DEBUG(authInfo.aclRead)
+        
         if not authInfo.checkAdmin():
             if AAA.checkAuthentication(schemaInfo.aaa) and not authInfo.checkReadACL(schemaInfo.sref): raise EpException(403, 'Forbidden')
             if AAA.checkAccount(schemaInfo.aaa): filter = f'{filter} AND owner:{authInfo.username}'
